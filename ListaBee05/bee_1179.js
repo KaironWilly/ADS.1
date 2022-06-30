038d5f18f5f0a74dc8e0c144d4e3f3de5c39682f
KaironWilly <102197577+KaironWilly@users.noreply.github.com>
2022-06-29 23:24:36 -0300

    Update bee_1179.js

--- ListaBee05/bee_1179.js
+++ ListaBee05/bee_1179.js
@@ -1,26 +1,41 @@
-import fs from 'fs'
-const input = fs.readFileSync('bee_1179_input.txt', 'utf-8')
-const lines = input.split('\n').map(Number)
-    //errada
 function main() {
-    const array_par = []
-    const array_impar = []
-    for (let i = 0; i < 15; i++) {
-        for (let cont = 0; cont < 5; cont++) {
-            if (lines[cont] % 2 === 0) {
-                array_par[1] = lines[cont]
-            } else {
-                array_impar[cont] = lines[cont]
-            }
-            cont == 4 ? cont = 0 : cont = cont
-        }
+    let array_par = new Array()
+    let cont_par = 0
+    let array_impar = new Array()
+    let cont_impar = 0
 
-        if (array_par.length == 5) {
-            console.log(`par[${i}] = ${array_par[i]}`)
+    for (let i = 0; i < lines.length -1; i++) {
+        if (eh_par(lines[i])) {
+            array_par[cont_par] = lines[i]
+            cont_par++
+            if (array_par.length === 5) {
+                mostrar_vetor_com_palavra_estatica(array_par, "par")
+                array_par = new Array()
+                cont_par = 0
+            }
+
         } else {
-            console.log(`impar[${i}] = ${array_impar[i]}`)
+            array_impar[cont_impar] = lines[i]
+            cont_impar++
+            if (array_impar.length == 5) {
+                mostrar_vetor_com_palavra_estatica(array_impar, "impar")
+                array_impar = new Array()
+                cont_impar = 0
+            }
         }
     }
 
+    mostrar_vetor_com_palavra_estatica(array_impar, "impar")
+    mostrar_vetor_com_palavra_estatica(array_par, "par")
+}
+
+function eh_par(n) {
+    return n % 2 === 0
+}
+
+function mostrar_vetor_com_palavra_estatica(vetor, palavra) {
+    for (let i = 0; i < vetor.length; i++) {
+        console.log(`${palavra}[${i}] = ${vetor[i]}`)
+    }
 }
 main()
